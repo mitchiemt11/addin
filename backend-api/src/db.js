f347e26a7dd17c25274302d2338feb9cacf64b7f
Mitchell <mitchiemt11@gmail.com>
2025-07-04 04:59:11 +0200

    Fix login route handler

--- backend-api/src/db.js
+++ backend-api/src/db.js
@@ -2,7 +2,7 @@
 const Database = require('better-sqlite3')
 const bcrypt = require('bcrypt')
 
-const db = new Database('./data.db')
+const db = new Database('data.db')
 
 try {
   // Create tables if they don't exist
